73f39d2cc79824744e53e27e4a79a9aec748ab58
Elena <124627358+ElenaBaeLop@users.noreply.github.com>
2024-02-21 20:53:35 +0100

    Eliminacion tabla rol

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('username');
             $table->string('name');
-            $table->string('rol')->nullable();
+            $table->boolean('admin')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
