ec03e10f1b9036713ed06a9f76b4f605b5ee61e1
Elena <124627358+ElenaBaeLop@users.noreply.github.com>
2024-02-23 15:52:42 +0100

    Funcionalidad de Login y Logout

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,5 +4,10 @@
         <p>{{session('success')}}</p>
     </div>
 @endif
-
+<!--Mensaje bienvenida-->
+<span>Welcome, {{auth()->user()->name}}</span>
 <!--Logout-->
+<form action="/logout" method="POST">
+    @csrf
+    <button type="submit">Log Out</button>
+</form>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\RegistrerController;
+use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,13 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 //Ruta para login
-Route::get('/', function () {
-    return view('welcome');
-});
-//Ruta del perfil
+Route::get('/',[SessionsController::class, 'create'])->middleware('guest')->name('login');
+Route::post('/',[SessionsController::class, 'store'])->middleware('guest')->name('login');
+//Ruta del perfil ->middleware('auth')
 Route::get('/profile', function () {
     return view('profile');
-});
+})->middleware('auth');
 //Ruta Registro / ->middleware('guest') -> Estas rutas solo tienen logica cuando el usuario no está logueado,SOLO si eres un guest entras aqui (auth , caso contrario)
 Route::get('/register', [RegistrerController::class, 'create'])->middleware('guest');
 Route::post('/register', [RegistrerController::class, 'store'])->middleware('guest');
+//Ruta Logout
+Route::post('logout',[SessionsController::class, 'destroy'])->middleware('auth');
+Route::get('logout',[SessionsController::class, 'destroy'])->middleware('auth');//Para cuando intento acceder sin estar logueado
