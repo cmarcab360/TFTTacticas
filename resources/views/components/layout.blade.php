64df8c66abe81c110fcfc7ca2f275854611c8221
Elena <124627358+ElenaBaeLop@users.noreply.github.com>
2024-03-05 20:05:36 +0100

    Arreglado problemas con first movile

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,7 +1,10 @@
 <!doctype html>
 <head>
-    <meta charset="utf-8" />
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>TFT Tactics</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    
     <link href="css/main.css" rel="stylesheet">
     <link href="css/login.css" rel="stylesheet">
     <link href="css/header.css" rel="stylesheet">
