f80adcf1e6abf84302a30bae930df49cbb244af6
Elena <124627358+ElenaBaeLop@users.noreply.github.com>
2024-03-05 19:01:11 +0100

    Prueba

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,9 +1,14 @@
-<header>
+<header class="header">
     <!--Mensaje bienvenida-->
-    <span>Welcome, {{auth()->user()->name}}</span>
-    <!--Logout-->
-    <form action="/logout" method="POST">
-        @csrf
-        <button type="submit">Log Out</button>
-    </form>
+    <p class="header__welcome">Welcome, {{ucfirst(auth()->user()->name)}}</p>
+    <img src="/img/logo.png" alt="Logo TFTTactics" class="header__logo">
+    <div class="header__options">
+        <!--Boton crear equipo-->
+        <button class="header__options__create"><a href="/createTeam">Add team</a></button>
+        <!--Logout-->
+        <form action="/logout" method="POST" class="header__options__button">
+            @csrf
+            <button type="submit" class="header__options__button__logout">Log Out</button>
+        </form>
+    </div>
 </header>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,7 @@
     <title>TFT Tactics</title>
     <link href="css/main.css" rel="stylesheet">
     <link href="css/login.css" rel="stylesheet">
+    <link href="css/header.css" rel="stylesheet">
 </head>
 
 <body>
