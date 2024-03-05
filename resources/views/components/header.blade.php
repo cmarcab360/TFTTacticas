cc12b177fc517366264f9f848f526bd2170c16a7
Elena <124627358+ElenaBaeLop@users.noreply.github.com>
2024-03-05 21:41:31 +0100

    Css cabecera first movile Retoques en el css del login

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,10 @@
 <header class="header">
     <!--Mensaje bienvenida-->
     <p class="header__welcome">Welcome, {{ucfirst(auth()->user()->name)}}</p>
-    <img src="/img/logo.png" alt="Logo TFTTactics" class="header__logo">
+    <picture class="header__picture">
+        <source media="(min-width: 700px)" srcset="/img/logo.png">
+        <img src="/img/logo2.png" alt="Logo TFTTactics" class="header__picture__logo">
+    </picture>
     <div class="header__options">
         <!--Boton crear equipo-->
         <button class="header__options__create"><a href="/createTeam">Add team</a></button>
