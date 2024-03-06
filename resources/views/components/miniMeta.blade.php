da954aea9790298d2d53fd486af8e31fd08dad38
Elena <124627358+ElenaBaeLop@users.noreply.github.com>
2024-03-06 19:29:58 +0100

    Creacion del componente miniMeta que muestra la informacion de los equipos Meta resumida

---
+++ resources/views/components/miniMeta.blade.php
@@ -0,0 +1,11 @@
+<div id="menu" class="menu">
+        <div class="menu__title">
+            <p class="menu__title__p">Meta list</p>
+            <button id="toggleButton" class="menu__title__button"><i class="fa-light fa-plus"></i></button>
+        </div>
+        <div id="listadoMeta" class ="menu__list">
+
+        </div>
+    </div>
+
+    <script src="{{ asset('js/miniMeta.js') }}"></script>
